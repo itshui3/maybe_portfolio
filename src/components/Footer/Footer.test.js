f55f0f2e47f138441e8e10a565215f304976bded
Hui <xuhuizhu369@gmail.com>
2021-02-07 15:03:01 -0800

    add footer and header tests

---
+++ src/components/Footer/Footer.test.js
@@ -0,0 +1,10 @@
+
+import { render } from '@testing-library/react';
+import Footer from './Footer';
+// [0] pull in App
+
+test('renders learn react link', () => {
+    const FooterElement = render(<Footer />);
+  // [1] use rtl's render on App(within what scope?)
+    expect(FooterElement.baseElement);
+});
---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,10 @@
+
+import { render } from '@testing-library/react';
+import Header from './Header';
+// [0] pull in App
+
+test('renders learn react link', () => {
+    const HeaderElement = render(<Header />);
+  // [1] use rtl's render on App(within what scope?)
+    expect(HeaderElement.baseElement);
+});
